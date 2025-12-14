d17e0bdb7d98e17d9b139f0950454d963d1f78e1
Rutvik008-co <rutvikkamble016@gmail.com>
2025-12-15 02:21:47 +0530

    program04

---
+++ Assignments/Assignment40/program04.cpp
@@ -0,0 +1,33 @@
+//  Write a recursive program to display below pattern
+//  Input : 6
+//  A   B   C   D   E   F
+
+
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    static int iCnt = 0;
+    static char ch = 'A';
+
+    while(iCnt != iNo)
+    {
+        cout<<ch<<"\t";
+        iCnt++;
+        ch++;
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the value : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);    
+
+    return 0;
+}
