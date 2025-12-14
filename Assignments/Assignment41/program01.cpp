c50554827cf1f9af14dbf1b9d137e9ae8ec391e6
Rutvik008-co <rutvikkamble016@gmail.com>
2025-12-15 03:31:33 +0530

    program01

---
+++ Assignments/Assignment41/program01.cpp
@@ -0,0 +1,31 @@
+//  Write a recursive program to display below pattern
+//  Input : 5
+//  5   *   4   *   3   *   2   *   1   *
+
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    static int iCnt = iNo;
+
+    while(iCnt > 0)
+    {
+        cout<<iCnt<<"\t*\t";
+        iCnt--;
+        iNo--;
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the value : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);    
+
+    return 0;
+}
