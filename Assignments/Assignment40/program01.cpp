bfc5ad7bb5f71ac7f20340aa36ef07ee6c19368a
Rutvik008-co <rutvikkamble016@gmail.com>
2025-12-15 01:31:12 +0530

    program01

---
+++ Assignments/Assignment40/program01.cpp
@@ -0,0 +1,30 @@
+//  Write a recursive program to display below pattern
+//  Input : 5
+//  *   *   *   *   *
+
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    static int iCnt = 1;
+
+    while(iCnt <= iNo)
+    {
+        cout<<"*\t";
+        iCnt++;
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the value : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);    
+
+    return 0;
+}
