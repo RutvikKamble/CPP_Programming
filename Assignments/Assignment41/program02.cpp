bea6f9522335c3c3be0936d825c8a50f5fa39edc
Rutvik008-co <rutvikkamble016@gmail.com>
2025-12-15 03:32:21 +0530

    program02

---
+++ Assignments/Assignment41/program02.cpp
@@ -0,0 +1,31 @@
+//  Write a recursive program which accept number from user and return summation of that number
+//  Input : 789
+//  output : 24
+
+#include<iostream>
+using namespace std;
+
+int Summation(int iNo)
+{
+    static int iSum = 0;
+    while(iNo != 0)
+    {
+        iSum = iSum + (iNo % 10);
+        iNo = iNo / 10;
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter the number : ");
+    scanf("%d",&iValue);
+
+    iRet = Summation(iValue);
+
+    printf("The summetion of : %d", iRet);
+
+    return 0;
+}
