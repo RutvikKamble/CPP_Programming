6460a6f64b6f03571e482d34ece9b005dfe293f3
Rutvik008-co <rutvikkamble016@gmail.com>
2026-01-12 12:26:48 +0530

    program02

--- Assignments/Assignment54/program02.cpp
+++ Assignments/Assignment54/program02.cpp
@@ -1,8 +1,7 @@
-// Write generic to check whether array is sorted
+// Write generic program to find sum of all even element
 
 #include<iostream>
 using namespace std;
-
 
 template<class T>
 void Display(T *Arr, int iNo)
@@ -17,89 +16,29 @@ void Display(T *Arr, int iNo)
     cout<<"\n";
 }
 
-
 template<class T>
-void CheckSort(T *Arr, int iSize)
+T SumEven(T *Arr, int iSize)
 {
     int iCnt = 0;
-    
-    bool isAscending = true;
-    bool isDescending = true;
-
-    for(iCnt = 0; iCnt < iSize-1; iCnt++)
-    {
-        if(Arr[iCnt] > Arr[iCnt + 1])
-        {
-            isAscending = false;
-        }
-
-        if(Arr[iCnt] < Arr[iCnt + 1])
-        {
-            isDescending = false;
-        }
-    }
-
-    if(isAscending == true)
-    {
-        cout << "Array is sorted in Ascending order";
-    }
-    else if(isDescending == true)
-    {
-        cout << "Array is sorted in Descending order";
-    }
-    else
-    {
-        cout << "Array is not sorted";
-    }
-}
-
-/*
-template<class T>
-void CheckSort(T *Arr, int iSize)
-{
-    int iCnt = 0, iCheck = 0;
-    int iCount1 = 0, iCount2 = 0;
-
-    iCheck = Arr[0];
+    T iSum = T();
 
     for(iCnt = 0; iCnt < iSize; iCnt++)
     {
-        if(Arr[iCnt] >= iCheck)
+        if((Arr[iCnt] % 2) == 0)
         {
-            iCheck = Arr[iCnt];
-            iCount1++;
+            iSum = iSum + Arr[iCnt];
         }
     }
 
-    for(iCnt = 0; iCnt < iSize; iCnt++)
-    {
-        if(Arr[iCnt] <= iCheck)
-        {
-            iCheck = Arr[iCnt];
-            iCount2++;
-        }
-    }
-    
-    if(iCount1 == iSize)
-    {
-        cout<<"Array is sorted in the Ascending order";
-    }
-    else if (iCount2 == iSize)
-    {
-        cout<<"Array is sorted in the Decending order";
-    }
-    else
-    {
-        cout<<"Array is not sorted";
-    }   
+    return iSum;
 }
-*/
 
 int main()
 {
-    int *Arr = nullptr;
+    int *Arr = nullptr;  
     
-    int iCnt = 0, iSize = 0, iValue = 0;
+    int iCnt = 0, iSize = 0;
+    int iRet = 0;
 
     cout<<"Enter the number how many value that you print : ";
     cin>>iSize;
@@ -113,11 +52,10 @@ int main()
     }
 
     Display(Arr, iSize);
+    iRet = SumEven(Arr, iSize);
 
-    CheckSort(Arr, iSize);
-
-
+    cout<<"Sum of all even elements : "<<iRet;
+    
     delete []Arr;
-
     return 0;
 }
