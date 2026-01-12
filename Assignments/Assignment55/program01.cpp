44376466efb41cc99e109afa242e15b373a0b152
Rutvik008-co <rutvikkamble016@gmail.com>
2026-01-12 12:32:14 +0530

    program01

--- Assignments/Assignment55/program01.cpp
+++ Assignments/Assignment55/program01.cpp
@@ -1,4 +1,4 @@
-// Write generic program to cpoy one array into another array
+// Write generic program to to perform addition of two number
 
 #include<iostream>
 using namespace std;
@@ -17,110 +17,29 @@ void Display(T *Arr, int iNo)
 }
 
 template<class T>
-void CopyArray(T *Arr, T*Brr, int iSize)
+T Addition(T No1, T No2)
 {
-    int iCnt = 0;
+    T Ans = T();
 
-    for(iCnt = 0; iCnt < iSize; iCnt++)
-    {
-        Brr[iCnt] = Arr[iCnt];
-    }
-
-    //cout<<"Copied Array is : ";
-    //Display(Brr, iSize);
+    Ans = No1 + No2;
+    
+    return Ans;
 }
 
 int main()
 {
-    int *Arr = nullptr;
-    int *Brr = nullptr;    
-    
-    int iCnt = 0, iSize = 0, iValue = 0;
+    int iValue1 = 0, iValue2 = 0, iRet = 0;
 
-    cout<<"Enter the number how many value that you print : ";
-    cin>>iSize;
+    cout<<"Enter the first value : ";
+    cin>>iValue1;
 
-    Arr = new int[iSize];
-    Brr = new int[iSize];
+    cout<<"Enter the second value : ";
+    cin>>iValue2;
 
-    for(iCnt = 0; iCnt < iSize; iCnt++)
-    {
-        cout<<"Enter number : ";
-        cin>>Arr[iCnt];
-    }
+    iRet = Addition(iValue1,iValue2);
 
-    Display(Arr, iSize);
-    //Display(Brr,iSize);
-    CopyArray(Arr, Brr, iSize);
-    cout<<"Copied Array is : ";
-    Display(Brr, iSize);
+    cout<<"Additon of above two nubmers : "<<iRet;
 
-    delete []Arr;
-    delete []Brr;
 
     return 0;
 }
-
-
-/*
-
-template<class T>
-void Display(T *Arr, int iNo)
-{
-    int iCnt = 0;
-
-    for(iCnt = 0; iCnt < iNo; iCnt++)
-    {
-        cout<<Arr[iCnt];
-        cout<<"\t";
-    }
-    cout<<"\n";
-}
-
-template<class T>
-void CopyArray(T *Arr, int iSize)
-{
-    T *Brr = nullptr;
-
-    Brr = new T[iSize];
-
-    int iCnt = 0;
-
-    for(iCnt = 0; iCnt < iSize; iCnt++)
-    {
-        Brr[iCnt] = Arr[iCnt];
-    }
-
-    cout<<"Copied Array is : ";
-    Display(Brr, iSize);
-
-    delete []Brr;
-}
-
-int main()
-{
-    int *Arr = nullptr;
-    
-    int iCnt = 0, iSize = 0, iValue = 0;
-
-    cout<<"Enter the number how many value that you print : ";
-    cin>>iSize;
-
-    Arr = new int[iSize];
-
-    for(iCnt = 0; iCnt < iSize; iCnt++)
-    {
-        cout<<"Enter number : ";
-        cin>>Arr[iCnt];
-    }
-
-    Display(Arr, iSize);
-    CopyArray(Arr, iSize);
-
-
-    delete []Arr;
-
-    return 0;
-}
-
-*/
